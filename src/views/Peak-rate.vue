347c00d81abcb01bba4ee00c4b44f3170084ad17
liutao <liutao@e-lead.cn>
2020-10-15 11:36:54 +0800

    refactor: 修改峰值速率精度和单位

--- src/views/Peak-rate.vue
+++ src/views/Peak-rate.vue
@@ -141,9 +141,9 @@ export default {
         return "0 B";
       }
       var k = 1024, // or 1024
-        sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"],
+        sizes = ["bps", "Kbps", "Mbps", "Gbps", "Tbps", "Pbps", "Ebps", "Zbps", "Ybps"],
         i = Math.floor(Math.log(bytes) / Math.log(k));
-      return (bytes / Math.pow(k, i)).toPrecision(3) + " " + sizes[i];
+      return (bytes / Math.pow(k, i)).toFixed(3) + " " + sizes[i];
     },
     result() {
       var frameEle;
