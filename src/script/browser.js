ff188f40810f51c6e9ca3d5f8f991c3860dd7673
desertsurge <desertsurge@qq.com>
2022-06-21 08:22:51 +0800

    解决nr换算错误

--- src/script/browser.js
+++ src/script/browser.js
@@ -1,11 +1,11 @@
 function isWeixn() {
-    return true;
-    // var ua = navigator.userAgent.toLowerCase();
-    // if (false || ua.match(/MicroMessenger/i) == "micromessenger") {
-    //     return true;
-    // } else {
-    //     return false;
-    // }
+    // return true;
+    var ua = navigator.userAgent.toLowerCase();
+    if (false || ua.match(/MicroMessenger/i) == "micromessenger") {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 export default isWeixn
